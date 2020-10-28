053e2a67454e054fdce74a5f0336d675e80cdc15
p56849086 <591547273@qq.com>
2020-10-28 19:37:21 +0800

    页面修改

--- components/page/ArticleMark.vue
+++ components/page/ArticleMark.vue
@@ -7,10 +7,7 @@
 			</el-breadcrumb>
 		</div>
 		<div class="container">
-			<div class="plugins-tips">
-				mavonEditor：基于Vue的markdown编辑器。
-				访问地址：<a href="https://github.com/hinesboy/mavonEditor" target="_blank">mavonEditor</a>
-			</div>
+
 			<mavon-editor v-model="content" ref="md" @imgAdd="$imgAdd" @change="change" style="min-height: 600px" />
 			<el-button class="editor-btn" type="primary" @click="submit">提交</el-button>
 		</div>
